8c5214adc138b535db4d568828ab27a9151f7d8f
JEONG JINBO <igodokaz@gmail.com>
2021-11-18 15:36:06 +0900

    2.0~2.8

---
+++ app.js
@@ -0,0 +1,48 @@
+// // List
+// const daysOfWeek = ["mon", "tue", "wed"];
+// console.log(daysOfWeek[2])
+
+// // Object
+// const player = {
+//   name: "nico",
+//   points: 10,
+//   fat: true,
+// };
+
+// console.log(player);
+// player.fat = false;
+// player.points = player.points + 16;
+// player.lastName = "potato";
+// console.log(player);
+// console.log(player.points);
+
+// function
+
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
+
+// sayHello("nico", 28);
+// sayHello("dal", 32);
+// sayHello("lynn", 8);
+
+// function plus(firstNumber, secondNumber) {
+//   console.log(firstNumber + secondNumber);
+// }
+
+// function divide(a, b) {
+//   console.log(a / b);
+// }
+// plus(8, 60);
+// divide(98, 20);
+
+// const player = {
+//   name: "nico",
+//   sayHello: function (otherPersonsName) {
+//     console.log("Hello " + otherPersonsName + " nice to meet you");
+//   },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
+// player.sayHello("nico");
