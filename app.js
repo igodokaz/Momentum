e88d095c152f7016f31cfafd065bdc3ff3234e17
JEONG JINBO <igodokaz@gmail.com>
2021-11-18 17:51:20 +0900

    2.9~2.12

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
-// // List
-// const daysOfWeek = ["mon", "tue", "wed"];
-// console.log(daysOfWeek[2])
-
-// // Object
-// const player = {
-//   name: "nico",
-//   points: 10,
-//   fat: true,
-// };
-
-// console.log(player);
-// player.fat = false;
-// player.points = player.points + 16;
-// player.lastName = "potato";
-// console.log(player);
-// console.log(player.points);
-
-// function
-
-// function sayHello(nameOfPerson, age) {
-//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-// }
-
-// sayHello("nico", 28);
-// sayHello("dal", 32);
-// sayHello("lynn", 8);
-
-// function plus(firstNumber, secondNumber) {
-//   console.log(firstNumber + secondNumber);
-// }
-
-// function divide(a, b) {
-//   console.log(a / b);
-// }
-// plus(8, 60);
-// divide(98, 20);
-
-// const player = {
-//   name: "nico",
-//   sayHello: function (otherPersonsName) {
-//     console.log("Hello " + otherPersonsName + " nice to meet you");
+// const calculator = {
+//   plus: function (a, b) {
+//     console.log(a + b);
+//   },
+//   minus: function (a, b) {
+//     console.log(a - b);
+//   },
+//   times: function (a, b) {
+//     console.log(a * b);
+//   },
+//   divide: function (a, b) {
+//     console.log(a / b);
+//   },
+//   power: function (a, b) {
+//     console.log(a ** b);
 //   },
 // };
 
-// console.log(player.name);
-// player.sayHello("lynn");
-// player.sayHello("nico");
+// calculator.plus(2, 3);
+// calculator.times(2, 3);
+// calculator.minus(2, 3);
+// calculator.divide(2, 3);
+// calculator.power(2, 3);
+
+// const age = 96;
+
+// function calculateKrAge(ageOfForeigner) {
+//   ageOfForeigner + 2;
+//   return "hello";
+// }
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
+  },
+};
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(powerResult);
