2fb2717d9c88592580a17fa21186e14ad4062e21
JEONG JINBO <igodokaz@gmail.com>
2021-11-21 19:11:07 +0900

    3.0~3.6

--- test.js
+++ test.js
@@ -1,3 +1,34 @@
+console.dir(document);
+document.title;
+document.title = "Hi";
+document.body;
+document.location;
+document.getElementById("Title");
+
+//
+
+const title = document.getElementById("title");
+console.dir(title);
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
+
+//
+
+const title = document.getElementById("hello");
+const title = document.getElementsByClassName("hello");
+const title = document.getElementsByTagName("h1");
+const title = document.querySelector(".hello h1");
+const title = document.querySelector("div h1");
+const title = document.querySelector(".hello h1:first-child");
+const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector("#hello");
+const title = document.getElementById("#hello");
+const title = document.querySelector("#hello form");
+const title = document.querySelector(".hello:first-child h1");
+
+//
 const h1 = document.querySelector("div.hello:first-child h1");
 
 console.dir(h1);
@@ -38,3 +69,5 @@ window.addEventListener("resize", handleWindowResize);
 window.addEventListener("copy", handleWindowCopy);
 window.addEventListener("offline", handleWindowOffline);
 window.addEventListener("online", handleWindowOnline);
+
+//
