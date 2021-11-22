6e2406b20b31f7bae378669d5ff15ce836a6af1b
JEONG JINBO <igodokaz@gmail.com>
2021-11-22 14:24:49 +0900

    4.4~4.7

--- test.js
+++ test.js
@@ -108,3 +108,25 @@ function handleLinkClick(event) {
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
 //
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  // loginForm.classList.add("hidden");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+//
+localStorage.setItem("username", "nico");
+localStorage.removeItem("username");
+//
